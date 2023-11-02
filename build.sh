bd1be75dec688574e12f2dd7bd8bad3e6976baeb
stwind <stwindfy@gmail.com>
2023-11-02 17:20:28 +0800

    update build script

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -euxo pipefail
 
 git clone https://github.com/facebook/zstd.git /opt/zstd && cd /opt/zstd && git checkout "${ZSTD_VERSION}"
 
-V=1 ZSTD_LIB_MINIFY=1 ZSTD_NO_INLINE=1 \
+V=1 ZSTD_LIB_MINIFY=1 ZSTD_NO_INLINE=1 ZSTD_STRIP_ERROR_STRINGS=1 \
   ZSTD_LIB_COMPRESSION=0 ZSTD_LIB_DEPRECATED=0 \
   CFLAGS="-flto -fmerge-all-constants" \
   emmake make lib-release
@@ -18,7 +18,7 @@ V=1 ZSTD_LIB_MINIFY=1 ZSTD_NO_INLINE=1 \
 emcc -flto --closure 1 -Oz \
   -o /src/zstd.js \
   --memory-init-file 0 \
-  -s EXPORTED_FUNCTIONS="['_ZSTD_isError', '_ZSTD_getFrameContentSize', '_ZSTD_decompress', '_malloc', '_free']" \
+  -s EXPORTED_FUNCTIONS="['_ZSTD_isError', '_ZSTD_getFrameContentSize', '_ZSTD_decompress', '_ZSTD_createDCtx', '_ZSTD_initDStream', '_ZSTD_DStreamInSize', '_ZSTD_DStreamOutSize', '_ZSTD_decompressStream', '_malloc', '_free']" \
   -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap']" \
   -s MODULARIZE -s EXPORT_ES6 -s ENVIRONMENT=web -s WASM=1 \
   -s ALLOW_MEMORY_GROWTH=1 \
